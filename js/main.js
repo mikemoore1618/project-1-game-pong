e2eaccb538569951ddab7554735d09267ea3ccc9
Mike Moore <mikemoore1618@gmail.com>
2018-05-23 17:01:15 -0700

    both sides of gameboard log when point is scored, winner declared after 5 points

--- js/main.js
+++ js/main.js
@@ -8,15 +8,46 @@ const $paddle2 = $('#paddle2');
 const $paddle = $('.paddle');
 const $player1 = $('#player1');
 const $player2 = $('#player2');
-let $score = 0
+let score1 = 1
+let score2 = 1
 let ballRight = -1
 let ballDown = -1
 
+function addPoint1() {
+    score1 = score1++
+    $player1.text(score1)
+    score1++;
+    didPlayer1Win();
+}
+function didPlayer1Win () {
+    if (score1 >= 6) {
+        alert("Player 1 Wins!")};
+        resetGame()
+}
+function addPoint2() { 
+    score2= score2++
+    $player2.text(score2)
+    score2++;
+    didPlayer2Win();
+}
+function didPlayer2Win () {
+    if (score2 >= 6) {
+        alert("Player 2 Wins!")};
+        resetGame()
+        }
 
-var player1Points = 0;
-var player2Points = 0;
+function resetGame () {
+    $ball.css({left: 442, top: 300});
+    $paddle.css("top","250px");  
+    clearInterval(ballMove),//stop moving 
+    disableBallMove()
+    // $player1.text(0)
+    // $player2.text(0)
+}
 
-/////make paddles move and stop in boundaries
+function disableBallMove() {
+    $start.off("click", startGame)
+}
 
 $doc.keydown(function(e){
     let $pos1 = $paddle1.css('top');
@@ -48,80 +79,54 @@ $doc.keydown(function(e){
 })
 
 ///////////make ball move and bounce
-
-
-
 function moveBall()  {
 
     $ball.css({left: `+=${2 * ballRight}`, top:`+=${1 * ballDown}`});
 
     // top wall
     if($ball.offset().top <= $gameboard.offset().top) {
-        ballDown = 1
+        ballDown = 1;
     }
     //left wall
     if ($ball.offset().left <= $paddle1.offset().left + $paddle1.width()
         && $ball.offset().top + $ball.width()/2 <= $paddle1.offset().top + $paddle1.height()
         && $ball.offset().top + $ball.width()/2 >= $paddle1.offset().top ){
-        ballRight = 1
+        ballRight = 1;
     }
     // bottom wall
     if($ball.offset().top + $ball.width() >= $gameboard.offset().top + $gameboard.height()) {
-        ballDown = -1
+        ballDown = -1;
     }
     // right wall
     if ($ball.offset().left + $ball.width() >= $paddle2.offset().left
         && $ball.offset().top + $ball.width()/2 <= $paddle2.offset().top + $paddle2.height()
-        && $ball.offset().top + $ball.width()/2 >= $paddle2.offset().top ){
-        ballRight = -1
+        && $ball.offset().top + $ball.width()/2 >= $paddle2.offset().top){
+        ballRight = -1;
     }
 
-    // paddle
+    if ($ball.offset().left + $ball.width() <= $gameboard.offset().left) {
+        addPoint2()
+        ballRight = -1
+        serveBall()
+    }
+    if($ball.offset().left >= $gameboard.offset().left + $gameboard.width() ){
+        addPoint1()
+        ballRight = 1
+        serveBall()
+    }
 }
 
-function $startGame() {
-    setInterval(moveBall, 10); ////the interval is what was causing ball to bounce rapidly
+function serveBall () {
+    var randomTop = Math.round(Math.random() * $gameboard.height() - $ball.height())
+    $ball.css({left: 442, top: randomTop})
+}
+    
+function startGame() {
+    setInterval(moveBall, 1); ////the interval is what was causing ball to bounce rapidly
     $paddle.css("top","250px"); 
     }
 
-    $start.on("click", $startGame)
-
-/////START HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-/////works if comment below code out
-  
-
-//// 1. get ball to bounce off player 1 and 2 paddles
-
-
-//// 2. log a point on correct players side when ball hits left or right side of screen
-function scorePoint() {
-    const $ballPos = $ball.css('left');
-    const leftBoundLimit = 0;
-    const rightBoundLimit = 900;
-    
-    (parseInt($ballPos)) <= leftBoundLimit;
-    console.log('point!');
-
-    player2.text(score++);
-
-    (parseInt($ballPos)) >= rightBoundLimit;
-    console.log('point!');
-
-    player1.text(score++);
-}
-
-//// 3. reset ball after point is scored and go in opposite direction
-//// if else statement: 
-//// if direction is left, go right
-//// if direction is right, go left
-
-// //// 4. declare winner after 5 points are scored
-
-function declareWinner() {
-    score >= 5;
-    alert('winner!')
-}
+    $start.on("click", startGame)
+    //.off
 
 //// 5. add controls to game page
-
